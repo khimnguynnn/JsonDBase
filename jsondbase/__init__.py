c3feb9f9c6b98907651546d18831f2c0e3663bf2
Khiêm Nguyễn <khiemnguyen@Khiems-MacBook-Pro.local>
2023-12-05 13:11:09 +0700

    add version init

--- jsondbase/__init__.py
+++ jsondbase/__init__.py
@@ -1,3 +1,3 @@
 import database
 
-__version__= "1.0.0"
+__version__= "1.0.5"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jsondbase',
-    version='1.0.0',
+    version='1.0.5',
     author='khiemndd',
     description='like database with json file',
     long_description=open('README.md').read(),
